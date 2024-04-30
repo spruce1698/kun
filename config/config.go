07cb60eb8f9153187006f21440318c18e3e17d2d
spruce <99873080+spruce1698@users.noreply.github.com>
2024-04-30 15:53:32 +0800

    *) 优化 版本

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 var (
-	Version       = "1.0.1"
+	Version       = "1.0.2"
 	WireCmd       = "github.com/google/wire/cmd/wire@latest"
 	KunCmd        = "github.com/spruce1698/kun@latest"
 	RunExcludeDir = ".git,.idea,tmp,vendor"
