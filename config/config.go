00f153ad045250ca3076342571472264d3f33d6b
spruce <99873080+spruce1698@users.noreply.github.com>
2025-03-06 14:58:08 +0800

    v0.1.0

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 var (
-	Version       = "0.0.4"
+	Version       = "0.1.0"
 	WireCmd       = "github.com/google/wire/cmd/wire@latest"
 	KunCmd        = "github.com/spruce1698/kun@latest"
 	RunExcludeDir = ".git,.idea,tmp,vendor"
